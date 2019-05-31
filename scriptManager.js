ec1da1462d1c8c1986d64e0ecfda99b41de040fb
spaceface777 <spaceface7777@gmail.com>
2019-05-31 12:05:28 +0200

    Implement standardJS

--- scriptManager.js
+++ scriptManager.js
@@ -1,46 +1,43 @@
 const fs = require('fs')
 const path = require('path')
 
-const basePath = path.join(__dirname, 'DuckyScripts/')
-const WindowsPath = path.join(basePath, 'Windows/')
-const macOSPath = path.join(basePath, 'macOS/')
-const LinuxPath = path.join(basePath, 'Linux/')
-
 const scripts = {}
 
-function getMainScript(dir) {
-    let file = path.join(dir, 'main.txt')
-    let main = fs.readFileSync(file, 'utf8')
-    return path.join(dir, main)
+function getMainScript (dir) {
+  let file = path.join(dir, 'main.txt')
+  let main = fs.readFileSync(file, 'utf8')
+  return path.join(dir, main)
 }
 
-function getLastChild(dir) {
-    return dir.split(path.sep).pop().toLowerCase()
+function getLastChild (dir) {
+  return dir.split(path.sep).pop().toLowerCase()
 }
 
-function isDirectory(dir) {
-    return fs.lstatSync(dir).isDirectory()
+function isDirectory (dir) {
+  return fs.lstatSync(dir).isDirectory()
 }
 
-function isntHidden(dir) {
-    return !/(^|\/|\\|\\\\)\./.test(dir)
+function isntHidden (dir) {
+  return !/(^|\/|\\|\\\\)\./.test(dir)
 }
 
-function getSubDirs(dir) {
-    dir = !path.isAbsolute(dir) ? path.join(__dirname, dir) : dir
+function getSubDirs (dir) {
+  dir = !path.isAbsolute(dir) ? path.join(__dirname, dir) : dir
 
-    return fs.readdirSync(dir).map(name => path.join(dir, name)).filter(isDirectory).filter(isntHidden)
+  return fs.readdirSync(dir).map(name => path.join(dir, name)).filter(isDirectory).filter(isntHidden)
 }
 
-function readScripts() {
-    let OSs = getSubDirs('DuckyScripts/')
-    OSs.forEach(OS => {
-        scripts[getLastChild(OS)] = {}
-        let subdirs = getSubDirs(OS)
-        subdirs.forEach(res => {
-            scripts[getLastChild(OS)][getLastChild(res)] = getMainScript(res)
-        })
+function readScripts () {
+  let OSs = getSubDirs('DuckyScripts/')
+
+  OSs.forEach(OS => {
+    scripts[getLastChild(OS)] = {}
+
+    let subdirs = getSubDirs(OS)
+    subdirs.forEach(res => {
+      scripts[getLastChild(OS)][getLastChild(res)] = getMainScript(res)
     })
+  })
 }
 
 readScripts()
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 const scripts = require('./scriptManager')
 
 const app = express()
@@ -11,79 +12,76 @@ const TOKEN = process.env.TOKEN
 const listeners = {}
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('/index.html', function (req, res) {
-    res.sendFile(__dirname + './index.html')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.post('/register', function (req, res) {
-    if (req.body.os && req.body.script && req.body.name) {
-        let script = scripts[req.body.os.toLowerCase()][req.body.script.toLowerCase()]
+  if (req.body.os && req.body.script && req.body.name) {
+    let script = scripts[req.body.os.toLowerCase()][req.body.script.toLowerCase()]
 
-        if (!script) res.sendStatus(404) // Script not found
+    if (!script) res.sendStatus(404) // Script not found
 
-        listeners[req.body.name] = {
-            script: script,
-            active: false
-        } // Register listener
+    listeners[req.body.name] = {
+      script: script,
+      active: false
+    } // Register listener
 
-        res.sendStatus(201) // Created successfully
-    } else res.sendStatus(400)
+    res.sendStatus(201) // Created successfully
+  } else res.sendStatus(400)
 })
 
 app.post('/listen', function (req, res) {
-    if (req.body.name) {
-        let listener = listeners[req.body.name]
+  if (req.body.name) {
+    let listener = listeners[req.body.name]
 
-        if (!listener) {
-            res.sendStatus(404) // No listener registered
-        } else if (!listener.active) {
-            res.send(null) // Nothing to send
-        } else {
-            res.sendFile(listener.script)
-            listeners[req.body.name].active = false 
-        }
-    } else res.sendStatus(400)
+    if (!listener) {
+      res.sendStatus(404) // No listener registered
+    } else if (!listener.active) {
+      res.send(null) // Nothing to send
+    } else {
+      res.sendFile(listener.script)
+      listeners[req.body.name].active = false
+    }
+  } else res.sendStatus(400)
 })
 
-
 app.post('/getListeners', function (req, res) {
-    if (req.body.token === TOKEN) {
-        res.send(listeners)
-    } else res.sendStatus(403)
+  if (req.body.token === TOKEN) {
+    res.send(listeners)
+  } else res.sendStatus(403)
 })
 
 app.post('/activateListener', function (req, res) {
-    if (req.body.token === TOKEN) {
-        if (listeners[req.body.name]) {
-            listeners[req.body.name].active = true
-            res.sendStatus(200)
-        } else res.sendStatus(404)
-    } else res.sendStatus(403)
+  if (req.body.token === TOKEN) {
+    if (listeners[req.body.name]) {
+      listeners[req.body.name].active = true
+      res.sendStatus(200)
+    } else res.sendStatus(404)
+  } else res.sendStatus(403)
 })
 
 app.post('/deactivateListener', function (req, res) {
-    if (req.body.token === TOKEN) {
-        if (listeners[req.body.name]) {
-            listeners[req.body.name].active = false
-            res.sendStatus(200)
-        } else res.sendStatus(404)
-    } else res.sendStatus(403)
+  if (req.body.token === TOKEN) {
+    if (listeners[req.body.name]) {
+      listeners[req.body.name].active = false
+      res.sendStatus(200)
+    } else res.sendStatus(404)
+  } else res.sendStatus(403)
 })
 
 app.post('/deleteListener', function (req, res) {
-    if (req.body.token === TOKEN) {
-        if (listeners[req.body.name]) {
-            delete listeners[req.body.name]
-            res.sendStatus(200)
-        } else res.sendStatus(404)
-
-    } else res.sendStatus(403)
+  if (req.body.token === TOKEN) {
+    if (listeners[req.body.name]) {
+      delete listeners[req.body.name]
+      res.sendStatus(200)
+    } else res.sendStatus(404)
+  } else res.sendStatus(403)
 })
 
-
 app.listen(PORT, function () {
-    console.log('Started on port ' + PORT)
+  console.log('Started on port ' + PORT)
 })
